496823424133748978491887223d60a4412e8ec5
Le Toan <letoan@gmail.com>
2020-10-25 12:01:43 +0700

    update code

---
+++ function.php
@@ -0,0 +1,36 @@
+<?php 
+function getName(){
+    // echo "my name is Bao";
+    // return 3;
+    $x = 6;
+    $y = 2;
+    if($x > $y){
+        return 0;
+        $x = 200;
+        echo $x;
+    } else {
+
+        return 0;
+        $x = 200;
+        echo $x;
+    }
+}
+
+// 1. function co return,
+// 2. function ko co return
+
+// getName();
+// getName();
+// getName();
+// getName();
+// getName();
+// getName();
+// getName();
+// getName();
+
+// tinh dong goi
+// tai su dung code
+// tinh bao mat
+
+$num = getName();
+var_dump($num);
